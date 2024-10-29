be44e29880ff1c740c7a2619985e8124a39c38b3
juanexplosions <juanfeperaltaz@gmail.com>
2024-10-29 10:03:42 -0500

    minor changes

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -14,7 +14,7 @@ export default function GameCard({ image, title, genre, description }) {
       <div className="card__info-container">
         <div className="card__title-container">
           <h3 className="card__title">{title}</h3>
-          <Icon className="card__star" icon="dashicons:star-empty" /> {/* star-filled */}  
+          <Icon className="card__star" icon="dashicons:star-empty" /> {/* star-filled */}
         </div>
         <p className="card__genre">{genre}</p>
         <p className="card__description">{description}</p>
